ad2b49538fb6ca619bbef2235de7149a0acb23eb
Ruslan <saharcorez@gmail.com>
2024-05-02 17:11:34 +0300

    private-review

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,24 @@
 window.addEventListener("DOMContentLoaded", function () {
+  const privatesGalleryThumbnail = new Swiper('.privates-gallery__thumbnails-container', {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+      600: {
+        slidesPerView: 6,
+      },
+    },
+  });
 
+  const privatesGallery = new Swiper('.privates-gallery__slider-container', {
+    navigation: {
+      nextEl: '.privates-gallery-btn-next',
+      prevEl: '.privates-gallery-btn-prev',
+    },
+    thumbs: {
+      swiper: privatesGalleryThumbnail
+    }
+  });
 })

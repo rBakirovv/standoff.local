7d73ff50d257be5c009bf367af68954dad39d82f
Ruslan <saharcorez@gmail.com>
2024-05-07 19:15:10 +0300

    remove cropped-text & new lists margins

--- js/cropped-text.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", function () {
-  const textItems = document.querySelectorAll("[data-rows]");
-
-  textItems.forEach((item) => {
-    item.style.webkitLineClamp = parseInt(item.dataset.rows);
-  })
-})

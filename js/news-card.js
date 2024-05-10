be250e7f302faf88271a8651f4a4754f196df073
Ruslan <saharcorez@gmail.com>
2024-05-10 15:48:00 +0300

    blog & news modals

--- js/news-card.js
+++ js/news-card.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", function () {
   const body = document.querySelector("body");
   const newsCardButtons = document.querySelectorAll(".news-card__button");
+  const newsCards = document.querySelectorAll(".news-card");
 
   newsCardButtons.forEach(element => {
     body.insertAdjacentHTML('beforeEnd',
@@ -12,4 +13,30 @@ window.addEventListener("DOMContentLoaded", function () {
       </div>
       `);
   });
+
+  newsCards.forEach((element) => {
+    const newsCardTextContainer = element.querySelector(".news-card__text-container");
+    const newsCardButton = element.querySelector(".news-card__button");
+
+    if (newsCardTextContainer.scrollHeight > newsCardTextContainer.offsetHeight) {
+      newsCardButton.classList.remove("dis-none");
+    } else {
+      newsCardButton.classList.add("dis-none");
+    }
+  })
+
+  window.addEventListener("resize", () => {
+    setTimeout(() => {
+      newsCards.forEach((element) => {
+        const newsCardTextContainer = element.querySelector(".news-card__text-container");
+        const newsCardButton = element.querySelector(".news-card__button");
+
+        if (newsCardTextContainer.scrollHeight > newsCardTextContainer.offsetHeight) {
+          newsCardButton.classList.remove("dis-none");
+        } else {
+          newsCardButton.classList.add("dis-none");
+        }
+      })
+    }, 500)
+  })
 })
